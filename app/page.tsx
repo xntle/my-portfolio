a159b5edbde72e8fd93bcf5edec8094376e1afc0
Thai An Le <115895584+xntle@users.noreply.github.com>
2025-02-21 18:30:22 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,6 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         {/* <Clients /> */}
